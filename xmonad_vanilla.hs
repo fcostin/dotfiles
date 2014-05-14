fa254544429f249281a6b5d6ec8de6e39d0cfeb6
rfc <reuben.fletcher-costin@biarri.com>
2014-05-14 10:40:44 +1000

    prevent applications stealing meta

--- xmonad_vanilla.hs
+++ xmonad_vanilla.hs
@@ -2,7 +2,17 @@
 -- ref: http://www.kunday.com/post/35700432761/barebones-xmonad-config-for-ubuntu
 
 import XMonad
+import qualified Data.Map as M
 
 main = xmonad defaultConfig
-    { modMask = mod4Mask -- use windows key as meta
+    { modMask = mod4Mask -- use windows key as meta, not alt.
+    , keys = newKeys
     }
+
+-- we need to do this otherwise we blow away all the default key config
+-- which is... undesirable.
+
+newKeys x = myKeys x `M.union` keys defaultConfig x
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList [((0, xK_Super_L), return ())] -- prevent any application (eg VIRTUALBOX!) from capturing the mod key
+
